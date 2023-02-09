12839e8c673e984035bd3670981879b45091b730
zoneAnaS <shaikhanas4321@gmail.com>
2023-02-09 15:46:57 +0530

    nav bar responsive collaspe

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,5 +1,6 @@
 let nav_btn = document.querySelector("#nav-btn");
 let nav_bar_icon=document.querySelector("#nav-bar i");
+let nav_links=document.querySelectorAll(".nav-link");
 
 nav_btn.addEventListener("change",function(){
     if(this.checked){
@@ -11,4 +12,11 @@ nav_btn.addEventListener("change",function(){
         nav_bar_icon.classList.remove("fa-xmark");
     }
     
+})
+nav_links.forEach((el)=>{
+    el.addEventListener("click",()=>{
+        nav_btn.checked=false;
+        nav_bar_icon.classList.add("fa-bars");
+        nav_bar_icon.classList.remove("fa-xmark");
+    })
 })
